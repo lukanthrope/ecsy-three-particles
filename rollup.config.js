08502b03ea44bb59ca164ca6547055a58a54a9c5
Slava «XiaNi» Solovjov <xianionline@gmail.com>
2020-07-15 10:22:59 +0300

    fixed transform and parenting?

--- rollup.config.js
+++ rollup.config.js
@@ -8,10 +8,13 @@ import pkg from "./package.json"
 export default [
   {
     input: "src/index.ts",
+    external: id => {
+      return ([ 'three', 'ecsy', 'ecsy-three', 'ecsy-input' ]).includes(id) || /^three\//.test(id) || /^troika-3d-text\//.test(id) || /^ecsy-three\//.test(id)
+    },
     plugins: [
       typescript(),
-      resolve(),
-      json({ exclude: ["node_modules/**", "examples/**"] }),
+      // resolve(),
+      // json({ exclude: ["node_modules/**", "examples/**"] }),
       // terser(),
       babel({ babelHelpers: "bundled" })
     ],
