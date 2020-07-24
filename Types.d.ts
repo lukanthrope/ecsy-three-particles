41fb46acb3c4f1534457d3da90e6ccd79646fbaa
lukanthrope <bydlocoder228@gmail.com>
2020-07-24 21:36:47 +0300

    Updates

--- Types.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const CustomTypes: {};
-export declare const FunctionType: import("ecsy").PropType<unknown, unknown>;
-export declare const VecXY: import("ecsy").PropType<unknown, unknown>;
-export declare const VecXYZ: import("ecsy").PropType<unknown, unknown>;
-export declare const RGB: import("ecsy").PropType<unknown, unknown>;
-export declare const Pointer: import("ecsy").PropType<unknown, unknown>;
--- systems/ParticleSystem.d.ts
+++
@@ -1,4 +0,0 @@
-import { System } from "ecsy";
-export declare class ParticleSystem extends System {
-    execute(deltaTime: any, time: any): void;
-}
